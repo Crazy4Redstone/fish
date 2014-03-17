3ad8f6a40b0ef9e07325e4e44a0e4e0460257788
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-17 14:45:00 -0500

    Update f12.js

--- f12.js
+++ f12.js
@@ -6,7 +6,7 @@
 ModPE.setItem (346,'fishing_rod_uncast',0,'Fishing Rod')
 ModPE.setItem (371,'gold_nugget',0,'Gold Nugget')
 ModPE.setItem (422,'dye_powder',3,'Coco Beans')
-ModPE.setItemCategory (371,3,0)
+ModPE.setItemCategory (371,6,0)
 
 // Foods
 
